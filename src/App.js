e19cf2a23189ea0326d79ec18a8a36a804d461ae
sherryvippro <sherryvippro@gmail.com>
2024-11-02 20:37:05 +0700

    create demo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const initItems = Array.from({ length: 1000 }, (_, i) => ({
+        id: i,
+        name: `Item ${i}`,
+    }))
+
+    const [items, setItems] = useState(initItems)
+
+    const deleteItem = (id) => {
+        setItems(items.filter((item) => item.id !== id))
+    }
+
+    const updateItem = (id) => {
+        setItems(
+            items.map((item) =>
+                item.id === id ? { ...item, name: item.name + ' (Updated) ' } : item,
+            ),
+        )
+    }
+
+    return (
+        <div className="App">
+            <h1>React Item List</h1>
+            <ul>
+                {items.map((item) => (
+                    <li key={item.id}>
+                        {item.name}
+                        <button onClick={() => deleteItem(item.id)}>Delete</button>
+                        <button onClick={() => updateItem(item.id)}>Update</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
-export default App;
+export default App
